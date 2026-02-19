20624bfa4b6a3f399a68e61d8ed70f5ed77b2082
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-02-19 18:37:26 +0000

    finalizado

---
+++ my-app/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>Olá professor!</h1>
+      <h3>tudo bem?</h3>
+
+      <h5>Jamais te abandonei</h5>
+      <h5>Estive nos seus dias difíceis</h5>
+      <h5>Sei tudo sobre você</h5>
+      <h5>Um amor que não muda</h5>
+      <h5>Sempre perto</h5>
+
+      <p><strong>Mensagem final: Ele ama você</strong></p>
+    </div>
+  );
+}
