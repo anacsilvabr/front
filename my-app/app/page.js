f062ffe98ba032dbabefb599b0bf878923d9f3bf
Ana Clara Brasil <anaclaradasilvabrasil8@gmail.com>
2026-02-19 18:49:27 +0000

    n

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col Smin-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       
-      <h4>Quem será? </h4>
+      <h4><strong>Quem será?</strong> </h4>
       <h5>Jamais te abandonei</h5>
       <h5>Estive nos seus dias difíceis</h5>
       <h5>Sei tudo sobre você</h5>
